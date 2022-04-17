221d946742d94e4eea9edbb1673e7ad0ed4d685d
PriyankaThirumaran <priyankamaran97@gmail.com>
2022-04-17 23:13:14 +0530

    file name modified

---
+++ Employee_Data_Management/src/main/java/com/company/service/AddressService.java
@@ -0,0 +1,5 @@
+package com.company.service;
+
+public interface AddressService {
+
+}
---
+++ Employee_Data_Management/src/main/java/com/company/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.company.service;
+
+import java.util.List;
+
+import com.company.domain.Employee;
+
+public interface EmployeeService {
+	
+	//Fetching all employee details
+	public List<Employee> fetchAllEmployee();
+	
+	//Fetching an employee
+	public Employee fetchEmployee(int empId);
+	
+	//Adding an employee
+	public String createEmployee(Employee employee);
+	
+	//Updating an employee
+	public String updateEmployee(int empId, Employee employee);
+	
+	//Deleting an employee
+	public String deleteEmployee(int empId);
+
+}
